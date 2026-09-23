81ea9b5099af3919d451c1f945c50db50f91098f
Jesper <jesper.elovsson92@gmail.com>
2026-09-23 13:21:32 +0200

    Slight hiccup with icons

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -2,10 +2,10 @@
 export default function Footer(){
     return(        
             <footer>
-                <a href="https://www.facebook.com/jesper.elovsson.1" target="_blank"><i class="fa-brands fa-square-facebook"></i></a>
-                <a href="https://www.instagram.com/jesperelov" target="_blank"><i class="fa-brands fa-square-instagram"></i></a>
-                <a href="https://www.linkedin.com/in/jesper-elovsson-11150013a" target="_blank"><i class="fa-brands fa-linkedin"></i></a>
-                <a href="https://github.com/Muffinsmaniac" target="_blank"><i class="fa-brands fa-square-github"></i></a>
+                <a href="https://www.facebook.com/jesper.elovsson.1" target="_blank"><i className="fa-brands fa-square-facebook"></i></a>
+                <a href="https://www.instagram.com/jesperelov" target="_blank"><i className="fa-brands fa-square-instagram"></i></a>
+                <a href="https://www.linkedin.com/in/jesper-elovsson-11150013a" target="_blank"><i className="fa-brands fa-linkedin"></i></a>
+                <a href="https://github.com/Muffinsmaniac" target="_blank"><i className="fa-brands fa-square-github"></i></a>
             </footer>    
     )
 }
