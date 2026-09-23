09b0dd899ea548aae004744d4d8968c99fe23977
Jesper <jesper.elovsson92@gmail.com>
2026-09-23 13:16:43 +0200

    Buttons up and running.

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -2,10 +2,10 @@
 export default function Footer(){
     return(        
             <footer>
-                <i class="fa-brands fa-square-facebook"></i>
-                <i class="fa-brands fa-square-instagram"></i>
-                <i class="fa-brands fa-linkedin"></i>
-                <i class="fa-brands fa-square-github"></i>
+                <a href="https://www.facebook.com/jesper.elovsson.1" target="_blank"><i class="fa-brands fa-square-facebook"></i></a>
+                <a href="https://www.instagram.com/jesperelov" target="_blank"><i class="fa-brands fa-square-instagram"></i></a>
+                <a href="https://www.linkedin.com/in/jesper-elovsson-11150013a" target="_blank"><i class="fa-brands fa-linkedin"></i></a>
+                <a href="https://github.com/Muffinsmaniac" target="_blank"><i class="fa-brands fa-square-github"></i></a>
             </footer>    
     )
 }
--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -5,8 +5,12 @@ export default function Header(){
             <h1 className="my-name">Jesper Elovsson</h1>
             <h2 className="titel">Fullstack Developer</h2>
             <div className="buttons">
-                <button id="email-button" className="button"><i class="fa-solid fa-envelope"></i>Email</button>
-                <button id="linked-button" className="button"><i class="fa-brands fa-linkedin"></i>LinkedIn</button>
+                <a href="mailto:jesper.elovsson92@gmail.com">
+                    <button id="email-button" className="button" ><i class="fa-solid fa-envelope"></i>Email</button>
+                </a>
+                <a href="https://www.linkedin.com/in/jesper-elovsson-11150013a" target="_blank">
+                    <button id="linked-button" className="button"><i class="fa-brands fa-linkedin"></i>LinkedIn</button>
+                </a>
             </div>        
         </header>
     )
